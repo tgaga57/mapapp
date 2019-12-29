46c9d52c3553eaaef67bf123bf6697902c61aa52
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-12-29 17:34:55 +0900

    検索した場所が地図上に表示されるようになった

--- mymap/ViewController.swift
+++ mymap/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    //textField
+    @IBOutlet weak var inputText: UITextField!
+    // map
+    @IBOutlet weak var displayMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let serchKeyword = textField.text
+        // 検索したやつが入力されているか
+        print(serchKeyword!)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(serchKeyword!, completionHandler: { (placemarks,Error ) in
+        
+            if let placemarks = placemarks?.first {
+                if let location = placemarks.location {
+                    let targetCoordinate = location.coordinate
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = serchKeyword
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region = MKCoordinateRegion.init(center: targetCoordinate, latitudinalMeters: 500.0,longitudinalMeters: 500.0)
+                }
+            }
+        
+        })
+        return true
+    }
+    
 }
 
